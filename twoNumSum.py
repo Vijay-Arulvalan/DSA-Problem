4cefe5f10d6e34368214ab634c0ab3a433cccbc8
Vijay-Arulvalan <vijayarulvalan@gmail.com>
2020-09-03 20:27:41 +0530

    TwoNumSum problem method

--- twoNumSum.py
+++ twoNumSum.py
@@ -1,1 +1,37 @@
-print("Hello, World")
+#Method 1 
+
+def TwoNumSum(array, targetSum):
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i + 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return[firstNum, secondNum]
+    return[]
+
+#method 2 // hash table
+
+def TwoNumSum(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return[potentialMatch, num]
+        else:
+            nums[num]= True
+    return[]
+
+#method 3 to solve the proble using pointer
+
+def TwoNumSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return[array[left], array[right]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
